5e330224147a0753689dd504434f863bc791b2ab
AceZephyr <pokefan107@gmail.com>
2021-04-05 00:26:04 -0400

    implemented lazy updating and an icon

--- stepgraph.py
+++ stepgraph.py
@@ -12,9 +12,13 @@ FONT = None
 
 BACKGROUND_COLOR = (0, 0, 0)
 
+GRAPH_BACKGROUND_COLOR_1 = (0, 0, 0)
+
+GRAPH_BACKGROUND_COLOR_2 = (10, 10, 10)
+
 BORDER_LINE_COLOR = (255, 255, 255)
 
-GRIDLINE_COLOR = (24, 24, 24)
+GRIDLINE_COLOR = (30, 30, 30)
 
 BATTLE_CHECK_COLOR = (0, 128, 128)
 
@@ -24,7 +28,7 @@ LEFT_OFFSET = 36
 
 BOTTOM_OFFSET = 18
 
-START_STEP = Step(0, 0)
+START_STEP = Step(1, 0)
 
 STEP_PER_GRIDLINE = 2
 
@@ -42,9 +46,11 @@ MAX_TOP_DANGER = 65536
 
 Y_GRIDLINE_HEIGHT = 12
 
-DEFAULT_SCROLL_STEPS = 1
+DEFAULT_SCROLL_STEPS = STEP_PER_GRIDLINE
 
-HOLD_SHIFT_SCROLL_STEPS = 10
+HOLD_SHIFT_SCROLL_STEPS = 10 * DEFAULT_SCROLL_STEPS
+
+SELECTED_STEP = None
 
 
 def scroll_starting_step_by(steps):
@@ -93,12 +99,17 @@ def main():
     pygame.init()
     pygame.display.set_caption("Big Shoes")
 
+    icon = pygame.image.load("icon.png")
+    pygame.display.set_icon(icon)
+
     surface = pygame.display.set_mode(DEFAULT_SIZE, FLAGS)
     font = pygame.font.SysFont(FONT, 16)
 
     clock = pygame.time.Clock()
 
     running = True
+
+    update = True
 
     # main loop
     while running:
@@ -107,11 +118,13 @@ def main():
         for event in pygame.event.get():
 
             if event.type == pygame.VIDEORESIZE:
+                update = True
                 surface = pygame.display.set_mode((event.w, event.h), FLAGS)
 
             elif event.type == pygame.MOUSEWHEEL:
+                update = True
                 if (pygame.key.get_mods() & (pygame.K_LCTRL | pygame.K_RCTRL)) != 0:  # scroll in and out
-                    scale_top_danger(-event.y * DEFAULT_SCROLL_STEPS)
+                    scale_top_danger(-event.y)
                 elif (pygame.key.get_mods() & (pygame.K_LSHIFT | pygame.K_RSHIFT)) != 0:
                     scroll_starting_step_by(-event.y * HOLD_SHIFT_SCROLL_STEPS)
                 else:  # scroll left and right
@@ -121,63 +134,66 @@ def main():
                 if event.button in {1, 3}:
                     step = step_nearest_x_coordinate(event.pos[0])
                     if step is not None:
-                        encdata = steplogic.encounter_at_step(step)
-                        ycbd = y_coordinate_by_danger(surface, encdata[0])
-                        print(step, encdata, ycbd, event)
+                        update = True
+                        SELECTED_STEP = step
+
 
             elif event.type == pygame.QUIT:
                 running = False
 
-        # draw background
-        surface.fill(BACKGROUND_COLOR)
+        if update:
 
-        # gridlines
-        x = LEFT_OFFSET
-        gridline = 0
-        stepid = START_STEP.stepid
-        while x < surface.get_width():
-            pygame.draw.line(surface, GRIDLINE_COLOR, (x, 0), (x, surface.get_height() - BOTTOM_OFFSET), 1)
-            if gridline % X_GRIDLINES_PER_TEXT == 0:
-                txt = font.render(str(stepid), False, (255, 255, 255))
-                surface.blit(txt, (x - (txt.get_width() // 2), surface.get_height() - BOTTOM_OFFSET + 4))
-            x += X_GRIDLINE_WIDTH
-            stepid = (stepid + (STEP_PER_GRIDLINE * 2)) % 256
-            gridline += 1
+            # draw background
+            surface.fill(BACKGROUND_COLOR)
 
-        y = graph_height(surface)
-        while y > 0:
-            pygame.draw.line(surface, GRIDLINE_COLOR, (LEFT_OFFSET, y), (surface.get_width(), y))
-            txt = font.render(str(danger_by_y_coordinate(surface, y)), False, (255, 255, 255))
-            surface.blit(txt, (LEFT_OFFSET - txt.get_width() - 4, y - txt.get_height() // 2))
-            y -= Y_GRIDLINE_HEIGHT
+            # gridlines
+            x = LEFT_OFFSET
+            gridline = 0
+            stepid = START_STEP.stepid
+            while x < surface.get_width():
+                pygame.draw.line(surface, GRIDLINE_COLOR, (x, 0), (x, surface.get_height() - BOTTOM_OFFSET), 1)
+                if gridline % X_GRIDLINES_PER_TEXT == 0:
+                    txt = font.render(str(stepid), False, (255, 255, 255))
+                    surface.blit(txt, (x - (txt.get_width() // 2), surface.get_height() - BOTTOM_OFFSET + 4))
+                x += X_GRIDLINE_WIDTH
+                stepid = (stepid + (STEP_PER_GRIDLINE * 2)) % 256
+                gridline += 1
 
-        # battle checks
-        x = LEFT_OFFSET + X_GRIDLINE_WIDTH
-        step = copy.copy(START_STEP)
-        step.advance_steps(1)
-        while x < surface.get_width():
-            step_data = steplogic.encounter_at_step(step)
-            if step_data[0] < TOP_DANGER:
-                y = y_coordinate_by_danger(surface, step_data[0])
-                color = BATTLE_CHECK_PREEMPTIVE_COLOR if step_data[1] else BATTLE_CHECK_COLOR
-                pygame.draw.line(surface, color, (x, 0), (x, y), 3)
-            x += (X_GRIDLINE_WIDTH // STEP_PER_GRIDLINE)
+            y = graph_height(surface)
+            while y > 0:
+                pygame.draw.line(surface, GRIDLINE_COLOR, (LEFT_OFFSET, y), (surface.get_width(), y))
+                txt = font.render(str(danger_by_y_coordinate(surface, y)), False, (255, 255, 255))
+                surface.blit(txt, (LEFT_OFFSET - txt.get_width() - 4, y - txt.get_height() // 2))
+                y -= Y_GRIDLINE_HEIGHT
+
+            # battle checks
+            x = LEFT_OFFSET + (X_GRIDLINE_WIDTH // STEP_PER_GRIDLINE)
+            step = copy.copy(START_STEP)
             step.advance_steps(1)
+            while x < surface.get_width():
+                step_data = steplogic.encounter_at_step(step)
+                if step_data[0] < TOP_DANGER:
+                    y = y_coordinate_by_danger(surface, step_data[0])
+                    color = BATTLE_CHECK_PREEMPTIVE_COLOR if step_data[1] else BATTLE_CHECK_COLOR
+                    pygame.draw.line(surface, color, (x, 0), (x, y), 3)
+                x += (X_GRIDLINE_WIDTH // STEP_PER_GRIDLINE)
+                step.advance_steps(1)
 
-        # offset text
-        txt = font.render(str(START_STEP.offset), False, (255, 255, 255))
-        surface.blit(txt, (2, surface.get_height() - txt.get_height() - 2))
+            # offset text
+            txt = font.render(str(START_STEP.offset), False, (255, 255, 255))
+            surface.blit(txt, (2, surface.get_height() - txt.get_height() - 2))
 
-        # bottom border
-        pygame.draw.line(surface, BORDER_LINE_COLOR, (LEFT_OFFSET, surface.get_height() - BOTTOM_OFFSET),
-                         (surface.get_width(), surface.get_height() - BOTTOM_OFFSET), 2)
+            # bottom border
+            pygame.draw.line(surface, BORDER_LINE_COLOR, (LEFT_OFFSET, surface.get_height() - BOTTOM_OFFSET),
+                             (surface.get_width(), surface.get_height() - BOTTOM_OFFSET), 2)
 
-        # left border
-        pygame.draw.line(surface, BORDER_LINE_COLOR, (LEFT_OFFSET, 0),
-                         (LEFT_OFFSET, surface.get_height() - BOTTOM_OFFSET), 2)
+            # left border
+            pygame.draw.line(surface, BORDER_LINE_COLOR, (LEFT_OFFSET, 0),
+                             (LEFT_OFFSET, surface.get_height() - BOTTOM_OFFSET), 2)
 
-        pygame.display.update()
-        clock.tick(60)
+            pygame.display.update()
+            update = False
+        clock.tick(30)
 
 
 if __name__ == '__main__':
