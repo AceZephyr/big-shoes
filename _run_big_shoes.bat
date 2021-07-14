427137f4d64d32fa23d03e5fcd6df34b6fa08585
acezephyr <pokefan107@gmail.com>
2021-07-13 22:49:12 -0400

    updated script text

--- _run_big_shoes.bat
+++ _run_big_shoes.bat
@@ -1,7 +1,7 @@
 @echo off
 where python
 if %ERRORLEVEL% NEQ 0 (
-    echo Please install python 3.9 or later at https://www.python.org. Then run big_shoes_setup.
+    echo Please install python 3.9 or later (easiest way is from the Microsoft Store). Then run _setup_python.bat
     pause
     exit
 )
--- _setup_python.bat
+++ _setup_python.bat
@@ -1,7 +1,7 @@
 @echo off
 where python
 if %ERRORLEVEL% NEQ 0 (
-    echo Please install python 3.9 or later at https://www.python.org. Then run this script again.
+    echo Please install python 3.9 or later (easiest way is from the Microsoft Store). Then run this script again.
     pause
     exit
 )
