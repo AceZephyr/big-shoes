2ae3de567e1413c1e78fe934c42cd09bdb92d5e7
acezephyr <pokefan107@gmail.com>
2021-07-20 17:49:57 -0400

    fixed the scripts

--- _run_big_shoes.bat
+++ _run_big_shoes.bat
@@ -1,8 +1,2 @@
 @echo off
-where python
-if %ERRORLEVEL% NEQ 0 (
-    echo Please install python 3.9 or later (easiest way is from the Microsoft Store). Then run _setup_python.bat
-    pause
-    exit
-)
 python main_window.py
--- _setup_python.bat
+++ _setup_python.bat
@@ -1,8 +1,2 @@
 @echo off
-where python
-if %ERRORLEVEL% NEQ 0 (
-    echo Please install python 3.9 or later (easiest way is from the Microsoft Store). Then run this script again.
-    pause
-    exit
-)
 python -m pip install -U PySide6 pygame pywin32 pywin32-ctypes
